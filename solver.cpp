1288052ecefe5e09874ecfe10307d0f18a33847c
reutKeyshawn <reki8611@gmail.com>
2020-07-19 05:28:07 +0300

    update

--- solver.cpp
+++ solver.cpp
@@ -1,16 +1,34 @@
 #include "solver.hpp"
-
+#include <math.h>
+#include <iostream>
+#include <unordered_map>
 using namespace solver;
 using namespace std;
 
+// ostream &solver::operator<<(ostream &out, const solver::RealVariable &var)
+// {
+//     for (auto it = var.umap.begin(); it != var.umap.end(); it++)
+//     {
+
+//         out << it->second;
+//         if (it->first > 0)
+//         {
+//             out << "x^" << it->first;
+//         }
+
+//         out << " ";
+//     }
+//     return out;
+// }
+
 //both
-solver::RealVariable operator+(const RealVariable &x, const RealVariable &y)
+RealVariable solver::operator+(const RealVariable &x, const RealVariable &y)
 {
     RealVariable res;
 
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
     {
-        cout << it->first << "  " << it->second << endl;
+        // cout << it->first << "  " << it->second << endl;
         if (it->second != 0) // if there is a real number
         {
             res.umap[it->first] += it->second; // add to res.umap[degree] value of x in the same degree
@@ -19,7 +37,7 @@ solver::RealVariable operator+(const RealVariable &x, const RealVariable &y)
 
     for (auto it2 = y.umap.begin(); it2 != y.umap.end(); it2++)
     {
-        cout << it2->first << "  " << it2->second << endl;
+        // cout << it2->first << "  " << it2->second << endl;
         if (it2->second != 0) // if there is a real number
         {
             res.umap[it2->first] += it2->second; // add to res.umap[degree] value of x in the same degree
@@ -28,13 +46,13 @@ solver::RealVariable operator+(const RealVariable &x, const RealVariable &y)
     return res;
 }
 
-solver::RealVariable operator-(const RealVariable &x, const RealVariable &y)
+RealVariable solver::operator-(const RealVariable &x, const RealVariable &y)
 {
     RealVariable res;
 
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
     {
-        cout << it->first << "  " << it->second << endl;
+        // cout << it->first << "  " << it->second << endl;
         if (it->second != 0) // if there is a real number
         {
             res.umap[it->first] += it->second; // add to res.umap[degree] value of x in the same degree
@@ -43,7 +61,7 @@ solver::RealVariable operator-(const RealVariable &x, const RealVariable &y)
 
     for (auto it2 = y.umap.begin(); it2 != y.umap.end(); it2++)
     {
-        cout << it2->first << "  " << it2->second << endl;
+        // cout << it2->first << "  " << it2->second << endl;
         if (it2->second != 0) // if there is a real number
         {
             res.umap[it2->first] -= it2->second; // add to res.umap[degree] value of x in the same degree
@@ -52,7 +70,7 @@ solver::RealVariable operator-(const RealVariable &x, const RealVariable &y)
     return res;
 }
 
-solver::RealVariable operator*(const RealVariable &x, const RealVariable &y)
+RealVariable solver::operator*(const RealVariable &x, const RealVariable &y)
 {
     RealVariable res;
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
@@ -87,13 +105,13 @@ solver::RealVariable operator*(const RealVariable &x, const RealVariable &y)
     return res;
 }
 
-solver::RealVariable operator/(const RealVariable &x, const RealVariable &y)
+RealVariable solver::operator/(const RealVariable &x, const RealVariable &y)
 {
     return x;
 }
 
 /// left side:
-solver::RealVariable operator+(const RealVariable &x, const double num)
+RealVariable solver::operator+(const RealVariable &x, double num)
 {
     RealVariable res;
 
@@ -109,7 +127,7 @@ solver::RealVariable operator+(const RealVariable &x, const double num)
     return res;
 }
 
-solver::RealVariable operator-(const RealVariable &x, const double num)
+RealVariable solver::operator-(const RealVariable &x, double num)
 {
 
     RealVariable res;
@@ -126,7 +144,7 @@ solver::RealVariable operator-(const RealVariable &x, const double num)
     return res;
 }
 
-solver::RealVariable operator*(const RealVariable &x, const double num)
+RealVariable solver::operator*(const RealVariable &x, double num)
 {
     RealVariable res;
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
@@ -139,7 +157,7 @@ solver::RealVariable operator*(const RealVariable &x, const double num)
     return res;
 }
 
-solver::RealVariable operator/(const RealVariable &x, const double num)
+RealVariable solver::operator/(const RealVariable &x, double num)
 {
     RealVariable res;
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
@@ -152,10 +170,10 @@ solver::RealVariable operator/(const RealVariable &x, const double num)
     return res;
 }
 
-solver::RealVariable operator^(const RealVariable &x, const int num)
+RealVariable solver::operator^(const RealVariable &x, const double num)
 {
     RealVariable res;
-    switch (num)
+    switch ((int)num)
     {
     case 0:
         res.umap[0] += 1;
@@ -164,8 +182,39 @@ solver::RealVariable operator^(const RealVariable &x, const int num)
         res = x;
         break;
     case 2:
-        res = x * x;
-        break;
+    {
+        for (auto it = x.umap.begin(); it != x.umap.end(); it++)
+        {
+            for (auto it2 = x.umap.begin(); it2 != x.umap.end(); it2++)
+            {
+                double real_number_multi = it->second * it2->second;
+                int degree_after_adding = it->first + it2->first;
+                if (real_number_multi != 0)
+                {
+
+                    switch (degree_after_adding)
+                    {
+                    case 0:
+                        res.umap[0] += real_number_multi;
+                        break;
+                    case 1:
+                        res.umap[1] += real_number_multi;
+                        break;
+                    case 2:
+                    {
+                        res.umap[2] += real_number_multi;
+                        break;
+                    }
+                    default:
+                        throw("Degree is not allowed!");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    // res = x * x;
+    break;
     default:
         throw("illegal degree!");
         break;
@@ -174,7 +223,7 @@ solver::RealVariable operator^(const RealVariable &x, const int num)
 
 // right side:
 
-solver::RealVariable operator+(const double num, const RealVariable &x)
+RealVariable solver::operator+(double num, const RealVariable &x)
 {
     RealVariable res;
 
@@ -190,7 +239,7 @@ solver::RealVariable operator+(const double num, const RealVariable &x)
     return res;
 }
 
-solver::RealVariable operator-(const double num, const RealVariable &x)
+RealVariable solver::operator-(double num, const RealVariable &x)
 {
     RealVariable res;
 
@@ -205,7 +254,7 @@ solver::RealVariable operator-(const double num, const RealVariable &x)
     res.umap[0] -= num;
     return res;
 }
-solver::RealVariable operator*(const double num, const RealVariable &x)
+RealVariable solver::operator*(double num, const RealVariable &x)
 {
     RealVariable res;
     for (auto it = x.umap.begin(); it != x.umap.end(); it++)
@@ -217,8 +266,8 @@ solver::RealVariable operator*(const double num, const RealVariable &x)
     }
     return res;
 }
-
-solver::RealVariable operator==(const double num, const RealVariable &x)
+// Equation
+RealVariable solver::operator==(double num, const RealVariable &x)
 {
 
     RealVariable res;
@@ -226,15 +275,87 @@ solver::RealVariable operator==(const double num, const RealVariable &x)
     res.umap[0] -= num;
     return res;
 }
-solver::RealVariable operator==(const RealVariable &x, const double num)
+RealVariable solver::operator==(const RealVariable &x, double num)
 {
     RealVariable res;
     res = x;
     res.umap[0] -= num;
     return res;
 }
-solver::RealVariable operator==(const RealVariable &x, const RealVariable &y)
+RealVariable solver::operator==(const RealVariable &x, const RealVariable &y)
 {
-    RealVariable res = x - y;
-    return x - y;
+
+    RealVariable res;
+
+    for (auto it = x.umap.begin(); it != x.umap.end(); it++)
+    {
+        cout << it->first << "  " << it->second << endl;
+        if (it->second != 0) // if there is a real number
+        {
+            res.umap[it->first] += it->second; // add to res.umap[degree] value of x in the same degree
+        }
+    }
+
+    for (auto it2 = y.umap.begin(); it2 != y.umap.end(); it2++)
+    {
+        cout << it2->first << "  " << it2->second << endl;
+        if (it2->second != 0) // if there is a real number
+        {
+            res.umap[it2->first] -= it2->second; // add to res.umap[degree] value of x in the same degree
+        }
+    }
+    return res;
+
+    // RealVariable res = x - y;
+    // return res;
+}
+
+double solver::solve(const RealVariable &e)
+{
+    double a, b, c = 0;
+    float x1, x2, discriminant, realPart, imaginaryPart;
+    for (auto it = e.umap.begin(); it != e.umap.end(); it++)
+
+        switch (it->first)
+        {
+        case 0:
+
+            c = it->second;
+            cout << "c = " << c << endl;
+
+            break;
+        case 1:
+            b = it->second;
+            cout << "b = " << b << endl;
+
+            break;
+        case 2:
+            a = it->second;
+            cout << "a = " << a << endl;
+
+            break;
+        }
+
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0)
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Roots are real and different." << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+
+    else if (discriminant == 0)
+    {
+        cout << "Roots are real and same." << endl;
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        cout << "x1 = x2 =" << x1 << endl;
+    }
+    else
+    {
+        throw("There is no real solution!");
+    }
+    return x1;
 }
--- solver.hpp
+++ solver.hpp
@@ -1,3 +1,5 @@
+#ifndef SOLVER_A_MASTER_SOLVER_HPP
+#define SOLVER_A_MASTER_SOLVER_HPP
 #include <iostream>
 #include <unordered_map>
 using namespace std;
@@ -8,36 +10,41 @@ namespace solver
     class RealVariable
     {
     public:
-        unordered_map<int, double> umap = {{0, 0},
-                                           {1, 0},
-                                           {2, 0}};
+        unordered_map<int, double> umap;
+        RealVariable()
+
+        {
+            umap = {{0, 0},
+                    {1, 0},
+                    {2, 0}};
+        }
 
         //right
-        friend RealVariable operator-(const double num, const RealVariable &x);
-        friend RealVariable operator/(const double num, const RealVariable &x);
-        friend RealVariable operator+(const double num, const RealVariable &x);
-        friend RealVariable operator*(const double num, const RealVariable &x);
-        friend RealVariable operator^(const double num, const RealVariable &x);
+        // friend ostream &operator<<(ostream &out, const RealVariable &var);
+
+        friend RealVariable operator-(double num, const RealVariable &x);
+        friend RealVariable operator+(double num, const RealVariable &x);
+        friend RealVariable operator*(double num, const RealVariable &x);
 
         //left
-        friend RealVariable operator+(const RealVariable &x, const double num);
-        friend RealVariable operator-(const RealVariable &x, const double num);
-        friend RealVariable operator*(const RealVariable &x, const double num);
-        friend RealVariable operator/(const RealVariable &x, const double num);
-        friend RealVariable operator^(const RealVariable &x, const int num);
+        friend RealVariable operator+(const RealVariable &x, double num);
+        friend RealVariable operator-(const RealVariable &x, double num);
+        friend RealVariable operator*(const RealVariable &x, double num);
+        friend RealVariable operator/(const RealVariable &x, double num);
+        friend RealVariable operator^(const RealVariable &x, const double num);
 
         //both
         friend RealVariable operator+(const RealVariable &x1, const RealVariable &x);
         friend RealVariable operator-(const RealVariable &x1, const RealVariable &x);
-        friend RealVariable operator*(const RealVariable &x1, const RealVariable &x);
+        friend RealVariable operator*(const RealVariable &x, const RealVariable &y);
         friend RealVariable operator/(const RealVariable &x1, const RealVariable &x);
         // friend RealVariable operator^(const RealVariable &x1, const RealVariable &x);
         //relevant for solve function
-        friend RealVariable operator==(const double num, const RealVariable &x);
-        friend RealVariable operator==(const RealVariable &x, const double num);
-        friend RealVariable operator==(const RealVariable &x1, const RealVariable &x)
-        {
-        }
+        friend RealVariable operator==(double num, const RealVariable &x);
+        friend RealVariable operator==(const RealVariable &x, double num);
+        friend RealVariable operator==(const RealVariable &x1, const RealVariable &x);
     };
+    double solve(const RealVariable &e);
 
 }; // namespace solver
+#endif
